8066edf828b416b9ee91b422eec0af8e84e11009
mkg20001 <mkg20001@gmail.com>
2017-08-09 16:19:39 +0200

    O... M... G.... - TLS... WORKS. FOR REAL THIS TIME

--- zeronet.js
+++ zeronet.js
@@ -38,7 +38,8 @@ const defaults = {
     },
     protocol: {
       crypto: [
-        require("zeronet-crypto/secio")
+        require("zeronet-crypto/secio"),
+        require("zeronet-crypto/tls").tls_rsa
       ]
     }
   },
